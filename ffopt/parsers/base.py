1021e118e8c1188825315aec44d8e08694cfcd2c
Anton Domnin <a.v.daomnin@gmail.com>
2024-10-14 03:05:17 +0300

    fix extractors

--- ffopt/parsers/base.py
+++ ffopt/parsers/base.py
@@ -7,25 +7,25 @@ class BaseParser:
     def __init__(self, filepath: PurePath):
         self.filepath = filepath
         self._data: dict = {}
-        self.__extractors: dict = {}
+        self._extractors: dict = {}
 
     def parse(self) -> None:
         with open(self.filepath, 'r') as file:
             content = file.read()
 
-        for key, extractor in self.__extractors.items():
+        for key, extractor in self._extractors.items():
             self._data[key] = extractor(content)
 
     @property
     def extractors(self):
-        return self.__extractors
+        return self._extractors
 
     @extractors.setter
     def extractors(self, key: str, extractor) -> None:
-        self.__extractors[key] = extractor
+        self._extractors[key] = extractor
 
     def pop_extractor(self, key: str) -> None:
-        self.__extractors.pop(key, None)
+        self._extractors.pop(key, None)
 
     @property
     def data(self) -> dict:
