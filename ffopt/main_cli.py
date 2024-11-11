96707a10bc10a3b6aaa54af07abeb7d809d2412f
Anton Domnin <a.v.daomnin@gmail.com>
2024-11-11 07:14:59 +0300

    add warning igroreing

--- ffopt/main_cli.py
+++ ffopt/main_cli.py
@@ -1,12 +1,13 @@
-import pathlib
-import tomllib
+import warnings
+from pathlib import Path
 
 import click
 
 from ffopt.core.pipeline import OptimizationPipeline
+from ffopt.core.prepare import DataPreparer
 from ffopt.jobs.utils import process_res
 
-from ffopt.core.prepare import DataPreparer
+warnings.filterwarnings("ignore")
 
 
 @click.command()
@@ -21,6 +22,7 @@ def main(input_file):
     app = OptimizationPipeline.from_dict(data)
     res = app.optimize()
     process_res(res, input_file)
+
 
 if __name__ == '__main__':
     main()
